a3a292035a84fabb6967a1980f110d24ad0c18f1
Bruno do Nascimento Maciel <brunodonascimentomaciel@gmail.com>
2024-11-09 05:04:20 -0300

    fix schema validation interceptor

--- src/service_component/interceptors.clj
+++ src/service_component/interceptors.clj
@@ -2,7 +2,6 @@
   (:require [io.pedestal.http :as http]
             [io.pedestal.http.body-params :as body-params]
             [io.pedestal.interceptor :as pedestal.interceptor]
-            [humanize.schema :as h]
             [schema.core :as s])
   (:import (clojure.lang ExceptionInfo)))
 
@@ -24,10 +23,10 @@
               (let [validation-result (try (s/validate schema json-params)
                                            (catch ExceptionInfo e
                                              (when (= (:type (ex-data e)) :schema.core/error)
-                                               {:status 422
-                                                :body   {:error "invalid-params"
-                                                         :data  (get-in (h/ex->err e) [:unknown :error])}})))]
+                                               {:status  422
+                                                :headers {}
+                                                :body    {:error "invalid-params"}})))]
                 (if (and (= "invalid-params" (get-in validation-result [:body :error]))
-                         (422 (:status validation-result)))
+                         (= 422 (:status validation-result)))
                   (assoc context :response validation-result)
                   context)))}))
--- test/integration/service_component_test.clj
+++ test/integration/service_component_test.clj
@@ -1,17 +1,25 @@
 (ns service-component-test
-  (:require [clojure.test :refer :all]
+  (:require [cheshire.core :as json]
+            [clojure.test :refer :all]
             [integrant.core :as ig]
             [schema.test :as s]
             [io.pedestal.test :as test]
             [matcher-combinators.test :refer [match?]]
             [common-clj.integrant-components.config :as component.config]
             [common-clj.integrant-components.routes :as component.routes]
-            [service-component.core :as component.service]))
+            [service-component.interceptors :as interceptors]
+            [service-component.core :as component.service]
+            [schema.core :as schema]))
 
 (def routes [["/test" :get [(fn [{{:keys [config]} :components}]
                               {:status 200
                                :body   config})]
-              :route-name :test]])
+              :route-name :test]
+             ["/schema-validation-interceptor-test" :post [(interceptors/schema-body-in-interceptor {:test schema/Str})
+                                                           (fn [_]
+                                                             {:status 200
+                                                              :body   {:test :schema-ok}})]
+              :route-name :schema-validation-interceptor-test]])
 
 (def system-components
   {::component.config/config   {:path "test/resources/config.example.edn"
@@ -28,5 +36,16 @@
       (is (match? {:status 200
                    :body   "{\"service-name\":\"rango\",\"service\":{\"host\":\"0.0.0.0\",\"port\":8080},\"current-env\":\"test\"}"}
                   (test/response-for service-fn :get "/test" :headers {"authorization" "Bearer test-token"}))))
+
+    (testing "That we can't fetch the test endpoint without a valid schema"
+      (is (match? {:status 422
+                   :body   "{\"error\":\"invalid-params\"}"}
+                  (test/response-for service-fn :post "/schema-validation-interceptor-test")))
+
+      (is (match? {:status 200
+                   :body   "{\"test\":\"schema-ok\"}"}
+                  (test/response-for service-fn :post "/schema-validation-interceptor-test"
+                                     :headers {"Content-Type" "application/json"}
+                                     :body (json/encode {:test :ok})))))
 
     (ig/halt! system)))
